7a0557124c01a518fd09f639476198f4475b1c6b
Shavi Pathania <pathania.shavi@gmail.com>
2020-02-23 21:28:07 +0530

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,20 @@
+(function() {
+function inject(onInject) {
+	if (window.jQuery) {
+    var $ = jQuery;
+  	onInject($);
+  	return;
+  }
+	var el = document.createElement('script');
+  el.src = '//code.jquery.com/jquery-latest.min.js';
+  el.onload = function() {
+  	jQuery.noConflict();
+    var $ = jQuery;
+  	onInject($);
+  };
+  document.head.appendChild(el);
+}
+inject(function($) {
+	$('#exampleFormControlSelect1').val('2')
+})
+})()
